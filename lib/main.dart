1e209830b167701e0ad06d5a78305aee06c14614
muhammad9harraz <muhammad9harraz@gmail.com>
2023-06-21 21:42:21 +0800

    harrazs commit

--- lib/main.dart
+++ lib/main.dart
@@ -26,27 +26,32 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  String movieTitle = '';
+  String moviePoster = '';
+  String movieDate = '';
+
   final apiKey = 'ccd287a77249d372162613f50c9972e3';
-  final url = Uri.parse('https://api.themoviedb.org/3/search/movie');
+
   TextEditingController _movieNameController = TextEditingController();
 
   Future<void> searchMovie(String movieName) async {
-    final queryParams = {
-      'api_key': apiKey,
-      'query': movieName,
-    };
+    final url = Uri.parse(
+        'https://api.themoviedb.org/3/search/movie?query=$movieName&api_key=$apiKey');
 
-    final response = await http.get(url.replace(queryParameters: queryParams));
+    final response = await http.get(url);
 
     if (response.statusCode == 200) {
       final jsonResponse = jsonDecode(response.body);
 
-      final movieTitle = jsonResponse['title'];
-      final movieDirector = jsonResponse['director'];
+      final results = jsonResponse['results'][0];
 
-      // Update the UI with the movie details
-      print('Title: $movieTitle');
-      print('Director: $movieDirector');
+      movieTitle = results['title'];
+      moviePoster = results['poster_path'];
+      movieDate = results['release_date'];
+
+      print(movieTitle);
+      print(moviePoster);
+      print(movieDate);
     } else {
       print('Error: ${response.statusCode}');
     }
@@ -55,6 +60,7 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.blueGrey,
       appBar: AppBar(
         centerTitle: true,
         title: const Text('Home'),
@@ -73,7 +79,23 @@ class _HomePageState extends State<HomePage> {
               searchMovie(movieName);
             },
             child: const Text('Search'),
-          )
+          ),
+          //SizedBox(height: 10),
+          Container(
+            width: 200,
+            height: 200,
+            child: (moviePoster != null && moviePoster.isNotEmpty)
+                ? Image.network(
+                    'https://image.tmdb.org/t/p/w500$moviePoster',
+                    fit: BoxFit.cover,
+                  )
+                : Placeholder(),
+          ),
+          SizedBox(height: 20),
+          Text(
+            'Title: $movieTitle\n' 'Release Date: $movieDate',
+            style: TextStyle(color: Colors.white),
+          ),
         ],
       ),
     );
